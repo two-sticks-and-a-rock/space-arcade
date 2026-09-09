a4ec32faa79a540aed9898b33c53f20d827d9d70
ToyaDR <ToyaDR@users.noreply.github.com>
2026-09-08 20:14:07 -0700

    Fixing script errors and add prototyping scene
    Parse errors from copy-paste and actually setting the position

--- scenes/player.gd
+++ scenes/player.gd
@@ -7,15 +7,19 @@ func _ready():
 	screen_size = get_viewport_rect().size
 
 func _process(delta):
+	
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
+	if Input.is_action_pressed("move_back"):
 		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed("move_forward"):
 		velocity.y -= 1
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
+
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
