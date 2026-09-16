e47e63bfe5c7e073bd8e445c6d019eeef40cda6f
ToyaDR <ToyaDR@users.noreply.github.com>
2026-09-16 14:13:27 -0700

    Set player direction on release

--- scenes/player.gd
+++ scenes/player.gd
@@ -4,20 +4,21 @@ signal hit
 
 @export var speed = 400
 var screen_size
+var direction = Vector2.ZERO
 
 func _ready():
 	screen_size = get_viewport_rect().size
 
 func _process(delta):
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_back"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_forward"):
-		velocity.y -= 1
+	var velocity = direction
+	# if Input.is_action_pressed("move_right"):
+	# 	velocity.x += 1
+	# if Input.is_action_pressed("move_left"):
+	# 	velocity.x -= 1
+	# if Input.is_action_pressed("move_back"):
+	# 	velocity.y += 1
+	# if Input.is_action_pressed("move_forward"):
+	# 	velocity.y -= 1
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
--- scenes/slingshot.gd
+++ scenes/slingshot.gd
@@ -16,6 +16,7 @@ extends Line2D
     - TBD: Do we allow slow drift?
     - TBD: steering?
 """
+@onready var player = get_parent().get_node("player")
 
 var start_vector := Vector2.ZERO
 var end_vector := Vector2.ZERO
@@ -25,9 +26,13 @@ func _input(_event: InputEvent) -> void:
         # The moment the mouse click happens, save the start vector
         start_vector = get_global_mouse_position()
         end_vector = start_vector
-        # print(start_vector)
     if Input.is_action_pressed("click"):
         # While the player holds the button, update the end_vector
         end_vector = get_global_mouse_position()
         add_point(start_vector, 0)
         add_point(end_vector, 1)
+    if Input.is_action_just_released("click"):
+        # When the player releases, set the direction of the ship
+        player.direction = ((start_vector - end_vector)*2).normalized()*100
+
+        
