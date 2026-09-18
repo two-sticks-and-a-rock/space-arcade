0dccfe344938e1c1a46a74c86df03ffc8330bc2d
ToyaDR <ToyaDR@users.noreply.github.com>
2026-09-17 23:04:25 -0700

    make slingshot child of player

--- scenes/slingshot.gd
+++ scenes/slingshot.gd
@@ -16,23 +16,23 @@ extends Line2D
     - TBD: Do we allow slow drift?
     - TBD: steering?
 """
-@onready var player = get_parent().get_node("player")
+@onready var player = get_parent() as RigidBody2D
 
 var start_vector := Vector2.ZERO
 var end_vector := Vector2.ZERO
 
 func _input(_event: InputEvent) -> void:
-    if Input.is_action_just_pressed("click"):
-        # The moment the mouse click happens, save the start vector
-        start_vector = get_global_mouse_position()
-        end_vector = start_vector
-    if Input.is_action_pressed("click"):
-        # While the player holds the button, update the end_vector
-        end_vector = get_global_mouse_position()
-        add_point(start_vector, 0)
-        add_point(end_vector, 1)
-    if Input.is_action_just_released("click"):
-        # When the player releases, set the direction of the ship
-        player.direction = ((start_vector - end_vector)*2).normalized()
+	if Input.is_action_just_pressed("click"):
+		# The moment the mouse click happens, save the start vector
+		start_vector = get_global_mouse_position()
+		end_vector = start_vector
+	if Input.is_action_pressed("click"):
+		# While the player holds the button, update the end_vector
+		end_vector = get_global_mouse_position()
+		add_point(start_vector, 0)
+		add_point(end_vector, 1)
+	if Input.is_action_just_released("click"):
+		# When the player releases, set the direction of the ship
+		player.direction = ((start_vector - end_vector)*2).normalized()
 
-        
+		
