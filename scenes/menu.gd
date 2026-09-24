6f945ee732347c4e631d7981a65647aacdf386fa
ToyaDR <ToyaDR@users.noreply.github.com>
2026-09-23 20:32:16 -0700

    menu scene

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -5,4 +5,5 @@ func _ready():
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_pressed("pause"):
-		get_tree().paused = !get_tree().paused	
+		get_tree().paused = !get_tree().paused
+		
