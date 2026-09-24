ae4d8146e38f8ad72b6f24286535c7a208dff8f2
ToyaDR <ToyaDR@users.noreply.github.com>
2026-09-23 21:08:46 -0700

    handle quit and restart from menu

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -5,4 +5,11 @@ func _ready():
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_pressed("pause"):
+		visible = !visible
 		get_tree().paused = !get_tree().paused
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _on_restart_pressed():
+	get_tree().reload_current_scene()
